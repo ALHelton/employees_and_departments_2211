c8957e4a43f7c91a417ddf3ab6d9a1dc564da731
Andra Helton <andrahelton@gmail.com>
2023-01-13 09:37:06 -0700

    Add test for departments and add_department

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -4,6 +4,10 @@ require './lib/budget'
 
 RSpec.describe Budget do
   let(:budget) { Budget.new("2022") }
+  let(:customer_service) { Department.new("Customer Service") }
+  let(:sales) { Department.new("Sales") }
+  let(:marketing) { Department.new("Marketing") }
+
 
   describe "#initialize" do
     it "exists" do
@@ -12,6 +16,18 @@ RSpec.describe Budget do
 
     it "attributes" do
       expect(budget.year).to eq("2022")
+    end
+  end
+
+  describe "#departments and #add_department" do
+    it "lists departments in array" do
+      expect(budget.departments).to eq([])
+
+      budget.add_department(customer_service)
+      budget.add_department(sales)
+      budget.add_department(marketing)
+
+      expect(budget.departments).to eq([customer_service, sales, marketing])
     end
   end
 end
