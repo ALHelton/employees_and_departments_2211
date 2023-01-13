a19184f074ac097607ce5cfb706179c1e18bbb62
Andra Helton <andrahelton@gmail.com>
2023-01-13 09:45:36 -0700

    Add passing test for low_expenses

--- lib/budget.rb
+++ lib/budget.rb
@@ -9,4 +9,10 @@ class Budget
   def add_department(dept)
     @departments << dept
   end
+
+  def low_expenses
+    @departments.map do |department|
+      department if department.expenses < 500
+    end.compact
+  end
 end
