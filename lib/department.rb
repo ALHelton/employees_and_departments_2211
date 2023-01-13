d3af4cdcb5fefe3f28b6eba218a3b26dcbb484cb
Andra Helton <andrahelton@gmail.com>
2023-01-13 09:26:50 -0700

    Add passing test for expenses and expense

--- lib/department.rb
+++ lib/department.rb
@@ -1,12 +1,18 @@
 class Department
   attr_reader :name,
-              :employees
+              :employees,
+              :expenses
   def initialize(name)
     @name = name
     @employees = []
+    @expenses = 0
   end
 
   def hire(person)
     @employees << person
+  end
+
+  def expense(amt)
+    @expenses += amt
   end
 end
