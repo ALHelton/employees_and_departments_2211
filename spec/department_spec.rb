a71a9a5dfe69c56c39340bcda914236bda36eeaa
Andra Helton <andrahelton@gmail.com>
2023-01-13 09:22:41 -0700

    Add test for hire method

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -3,7 +3,11 @@ require './lib/employee'
 
 RSpec.describe Department do
   let(:customer_service) { Department.new("Customer Service") }
-  
+  let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"}) }
+  let(:aaron) { Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) }
+
+
+
   describe "#initialize" do
     it "exists" do
       expect(customer_service).to be_an_instance_of(Department)
@@ -12,6 +16,15 @@ RSpec.describe Department do
     it "attributes" do
       expect(customer_service.name).to eq("Customer Service")
       expect(customer_service.employees).to eq([])
+    end
+  end
+
+  describe "#hire" do
+    it "adds person to employees array" do
+      customer_service.hire(bobbi)
+      customer_service.hire(aaron) 
+
+      expect(customer_service.employees).to eq([bobbi, aaron])
     end
   end
 end
